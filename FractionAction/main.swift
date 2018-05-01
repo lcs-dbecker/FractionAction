5f8866bd498884ce0290bfc8b5843029394132ca
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2018-05-01 13:28:55 -0400

    done

--- FractionAction/main.swift
+++ FractionAction/main.swift
@@ -21,7 +21,7 @@ while 1 == 1 {
     //  Test #1 Wait for input and at the same time ensure input is not nil.
     guard let validInput = readLine() else {
         // If we got inside this structure we have nil input so we should not do any more tests, just prompt again.
-            continue
+        continue
     }
     
     // Test #2 Is it an integer ?
@@ -30,7 +30,7 @@ while 1 == 1 {
         // that cannot be made into an integer.
         continue // ensure
     }
-
+    
     // Test #3 is it less than 0
     if givenInteger < 0  {
         continue
@@ -43,30 +43,29 @@ while 1 == 1 {
 }
 var denominator = -1
 while 1 == 1 {
-
+    
     // Prompt for denominator?
     print("Denominator?")
+    
     
     // Test 1 Wait for denominator, ensure input is valid (not nil)
     guard let validInput = readLine() else{
         continue
     }
-   
+    
     // Test 2 Is it an integer
     guard let givenInteger = Int (validInput) else{
         continue
     }
-
+    
     //Test 3 is it less or greater than 1
     
-    if givenInteger >= 1 {
-        continue
+    if givenInteger > 0 {
         
-        }
-
-//if all tests passed continue (proceed)
-    denominator = givenInteger
-    break // stop while loop
+        //if all tests passed continue (proceed)
+        denominator = givenInteger
+        break // stop while loop
+    }
 }
 
 
@@ -79,22 +78,22 @@ let quotient = numerator / denominator
 
 //Is it a whole number
 if remainder == 0 {
-  
     
-// We have a whole number
+    
+    // We have a whole number
     print("The result is:")
     print(quotient)
 } else {
-// WE MUST REDUCE
-// loop backwards from the halfway point of the numerator
-// Loop backwards form halfway point of 8
-// 4,3,2
+    // WE MUST REDUCE
+    // loop backwards from the halfway point of the numerator
+    // Loop backwards form halfway point of 8
+    // 4,3,2
     var GCF = -1
     for i in stride(from: remainder / 2, to: 2, by: -1){
-  // When the counter vairable "i" evenly divides into both
+        // When the counter vairable "i" evenly divides into both
         // the remaijnder and the original denominator ... we have the GCF
         
-        if remainder % i == 0 && validDenominator % i == 0 {
+        if remainder % i == 0 && denominator % i == 0 {
             
             GCF = i
             break //stop the loop
@@ -104,11 +103,64 @@ if remainder == 0 {
     print(GCF)
     
     // Print the reduced
-
+    
 }
 
 // OUTPUT
 // Report results to the user here
+// Get the quotient and initialOutputNumerator
+let quotient1 = numerator / denominator               // 3
+let initialOutputNumerator = numerator % denominator // 8
+let initialOutputDenominator = denominator                   // 12
+
+print("The result is:")
+
+// Is the result a whole number?
+if initialOutputNumerator == 0 {
+    
+    // We have a whole number, we are done
+    print(quotient1)
+    
+} else {
+    
+    // We might have to reduce
+    
+    // FIRST: Check whether the initial output numerator is the GCF
+    if initialOutputDenominator % initialOutputNumerator == 0 {
+        
+        
+        // Get the reduced numerator and denominator
+        let reducedOutputNumerator = 1
+        let reducedOutputDenominator = initialOutputDenominator / initialOutputNumerator
+        
+        // Print the reduced fraction
+        print("\(quotient1) \(reducedOutputNumerator)/\(reducedOutputDenominator)")
+        
+    } else {
+        
+        // We must reduce by finding a GCF (greatest common factor) of the numerator and denominator
+        // Loop backwards from the halfway point of the numerator
+        var greatestCommonFactor = -1 // not the real GCF yet
+        for possibleFactor in stride(from: initialOutputNumerator / 2, through: 2, by: -1) {
+            
+            if initialOutputNumerator % possibleFactor == 0 && denominator % possibleFactor == 0 {
+                
+                // Now we know that "possibleFactor" is the greatest common factor (GCF)
+                greatestCommonFactor = possibleFactor
+                break // stop the loop
+            }
+        }
+        
+        // Get the parts of the reduced fraction
+        let reducedOutputNumerator = initialOutputNumerator / greatestCommonFactor
+        let reducedOutputDenominator = denominator / greatestCommonFactor
+        
+        // Print the reduced fraction
+        print("\(quotient1) \(reducedOutputNumerator)/\(reducedOutputDenominator)")
+        
+    }
+    
+}
 
 
 
